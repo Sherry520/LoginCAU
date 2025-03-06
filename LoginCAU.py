e28ffcff943ef2bdc74c128b30a110a6a0a267d5
Yuhang Guo <22561797+Sherry520@users.noreply.github.com>
2025-03-06 12:33:45 +0800

    Warning: Debug Version for Developer!!!

--- LoginCAU.py
+++ LoginCAU.py
@@ -1,258 +1,191 @@
-#!/usr/bin/python
-# coding=utf-8
-import pickle
-import base64
-import hashlib
-import httplib
-from urlparse import urljoin
+#!/usr/bin/env python3
+import sys
+import getpass
+import keyring
+import urllib.parse
 import requests
-import re
-# import sys
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
-import os
-import platform
-import time
-# from io import open
 
-# Following codes are copied from network
+# 服务名称（用于 keyring 存储）
+SERVICE_NAME = "CAU_Campus_Network"
 
-
-class AuthCode(object):
-
-    @classmethod
-    def encode(cls, string, key, expiry=0):
-        return cls._auth_code(string, 'ENCODE', key, expiry)
-
-    @classmethod
-    def decode(cls, string, key, expiry=0):
-        return cls._auth_code(string, 'DECODE', key, expiry)
-
-    @staticmethod
-    def _md5(source_string):
-        return hashlib.md5(source_string).hexdigest()
-
-    @classmethod
-    def _auth_code(cls, input_string, operation='DECODE', key='', expiry=3600):
-        rand_key_length = 4
-        key = cls._md5(key)
-        key_a = cls._md5(key[:16])
-        key_b = cls._md5(key[16:])
-        if rand_key_length:
-            if operation == 'DECODE':
-                key_c = input_string[:rand_key_length]
-            else:
-                key_c = cls._md5(str(time.time()))[-rand_key_length:]
-        else:
-            key_c = ''
-        crypt_key = key_a + cls._md5(key_a + key_c)
-        if operation == 'DECODE':
-            handled_string = base64.b64decode(input_string[rand_key_length:])
-        else:
-            expiration_time = expiry + int(time.time) if expiry else 0
-            handled_string = '%010d' % expiration_time + \
-                cls._md5(input_string + key_b)[:16] + input_string
-        rand_key = list()
-        for i in xrange(256):
-            rand_key.append(ord(crypt_key[i % len(crypt_key)]))
-        box = range(256)
-        j = 0
-        for i in xrange(256):
-            j = (j + box[i] + rand_key[i]) % 256
-            tmp = box[i]
-            box[i] = box[j]
-            box[j] = tmp
-        result = ''
-        a = 0
-        j = 0
-        for i in xrange(len(handled_string)):
-            a = (a + 1) % 256
-            j = (j + box[a]) % 256
-            tmp = box[a]
-            box[a] = box[j]
-            box[j] = tmp
-            result += chr(ord(handled_string[i])
-                          ^ (box[(box[a]+box[j]) % 256]))
-        if operation == 'DECODE':
-            if (int(result[:10]) == 0 or (int(result[:10]) - time.time() > 0)) and \
-                    (result[10:26] == cls._md5(result[26:] + key_b)[:16]):
-                output_string = result[26:]
-            else:
-                output_string = ''
-        else:
-            output_string = key_c + base64.b64encode(result)
-        return output_string
-
-
-def getLoginUrl():
-    # 地址跳转
-    netRes = testNet()
-    if netRes['code'] == 302:
-        if netRes['domain'] == 'go.microsoft.com':
-            return 'logged'
-        else:
-            return netRes['target']
-    else:
-        return 'error'
-
-
-def displayAuthor():
-    print "*********************************************************************"
-    print "*                Login CAU                                          *"
-    print "*Note: This program is only available for Python2                   *"
-    print "*Version:V0.1                                                       *"
-    print "*Author:Bruce Guo                                                   *"
-    print "*IF you find any bugs or you have some suggestions, please write    *"
-    print "*an ISSUE in https://github.com/Sherry520/LoginCAU                  *"
-    print "*********************************************************************"
-
-
-def fin():
-    inp = raw_input("enter to exit\n")
-    os._exit(0)
-
-
-def getPWD():
-    syst = platform.system()
-    if syst == "Windows":
-        return os.getcwdu()+'\\'
-    else:
-        return os.getcwdu()+'/'
-
-
-def checkPassFile(LoginURL, flag=1):
-    data = {}
-    data1 = {}
-    py_path = getPWD()
+def get_login_url():
+    """通过访问 http://www.msftconnecttest.com/redirect 获取认证网关地址"""
     try:
-        if flag == 1:
-            f = open(py_path+'data.pkl', 'r')
-            data = pickle.load(f)
-            f.close()
-            if data['ID'] == '' or data['Pass'] == '':
-                raise ValueError
-            else:
-                # 如果只缺少Url信息，则在登陆时添加
-                if ('Url' not in data.keys()) or data['Url'] == "WrongURL" or data['Url'] == '' or data['Url'] == 'logged':
-                    data['Url'] = LoginURL
-                    f = open(py_path+'data.pkl', 'wb')
-                    pickle.dump(data, f)
-                    f.close()
-                tmp = AuthCode.decode(data['Pass'], data['ID'])
-                data['Pass'] = tmp
-        if flag == 0:
-            os.unlink(py_path+'data.pkl')
-            raise ValueError
-    # 抛出异常后，才会运行下面代码
-    except Exception, e:
-        if flag == 1:
-            print "It seems that you havn't save your ID and Password or the File is destroyed, \n please input your ID and Password. And these will be encrypted."
+        response = requests.get("http://www.msftconnecttest.com/redirect", allow_redirects=False)
+        if response.status_code == 302:  # 重定向状态码
+            location = response.headers.get("Location", "")
+            if location:
+                print(f"检测到认证网关地址: {location}")
+                return location
+        print("错误: 无法获取认证网关地址，请检查网络连接")
+    except requests.exceptions.RequestException as e:
+        print(f"网络请求失败: {str(e)}")
+    return None
+
+def display_author():
+    """显示作者信息"""
+    print("*********************************************************************")
+    print("*                Secure CAU Campus Network Login                   *")
+    print("* Security Features:                                                *")
+    print("*  - Python 3 兼容版本                                              *")
+    print("*  - 使用系统密钥库存储凭据                                         *")
+    print("*  - 密码输入无回显                                                 *")
+    print("*********************************************************************")
+
+def get_credentials():
+    """从系统密钥库获取或输入账号密码"""
+    user_id = keyring.get_password(SERVICE_NAME, "last_user_id")
+    
+    if user_id:
+        password = keyring.get_password(SERVICE_NAME, user_id)
+        if password:
+            return user_id, password
+    
+    # 交互式输入
+    print("\n首次使用或需要更新凭据：")
+    user_id = input("校园网账号: ").strip()
+    while not user_id:
+        print("账号不能为空！")
+        user_id = input("校园网账号: ").strip()
+    
+    password = getpass.getpass("登录密码: ")
+    while not password:
+        print("密码不能为空！")
+        password = getpass.getpass("登录密码: ")
+    
+    # 存储凭据
+    keyring.set_password(SERVICE_NAME, "last_user_id", user_id)
+    keyring.set_password(SERVICE_NAME, user_id, password)
+    return user_id, password
+
+def secure_request(url, params):
+    """发送 HTTP 请求"""
+    encoded_params = urllib.parse.urlencode(params)
+    full_url = f"{url}?{encoded_params}"
+    print(f"调试: 请求地址: {full_url}")  # 打印完整的请求地址
+    
+    try:
+        response = requests.get(full_url)
+        print(f"调试: 响应状态码: {response.status_code}")  # 打印响应状态码
+        print(f"调试: 响应内容: {response.text}")  # 打印响应内容
+        return response.status_code == 200
+    except requests.exceptions.RequestException as e:
+        print(f"请求失败: {str(e)}")
+        return False
+
+def network_login(login_url, user_id, password):
+    """执行登录操作"""
+    # 构造完整的登录URL
+    login_path = "/drcom/login"
+    full_login_url = urllib.parse.urljoin(login_url, login_path)
+    
+    login_params = {
+        "callback": "dr1003",
+        "DDDDD": f"{user_id}@cau",  # 账号需要附加 @cau
+        "upass": password,          # 密码直接使用，URL 编码会自动处理特殊字符
+        "0MKKey": "123456",
+        "R1": "0",
+        "R3": "0",
+        "R6": "0",
+        "para": "00",
+        "v6ip": "",
+        "v": "3124"  # 添加 v 参数
+    }
+    
+    if secure_request(full_login_url, login_params):
+        print("登录请求已发送，验证状态...")
+        if check_login_status():
+            print("登录成功")
+            return True
         else:
-            print "Please re-input your ID and Password"
-            #data1['Url'] = LoginURL
-        ID = raw_input('input your ID:')
-        Pass = raw_input("input your password:")
-        data = {'ID': ID, 'Pass': Pass, 'Url': LoginURL}
-        EncodePass = AuthCode.encode(Pass, ID)
-        data1['ID'] = data['ID']
-        data1['Pass'] = EncodePass
-        data1['Url'] = LoginURL
-        f = open(py_path+'data.pkl', 'wb')
-        pickle.dump(data1, f)
-        f.close()
-    return data
-
-
-def testNet():
-    data = {}
-    conn = httplib.HTTPConnection("www.msftconnecttest.com")
-    conn.request('GET', '/redirect', headers={
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        # "Connection": "keep-alive",
-        "Host": "www.msftconnecttest.com",
-                # "Upgrade-Insecure-Requests": "1",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-    })
-    res = conn.getresponse()
-    target = res.getheader("location")
-    domain = re.findall(ur'^(?:https?:\/\/)?([^\/]+)', target)
-    code = res.status
-    conn.close()
-    data = {'code': code, 'target': target, 'domain': domain[0]}
-    return data
-
-
-def netLogIn(LoginURL, logName, logPass):
-    # 登录参数，除了用户名和密码之外的参数，不用全写上，不然可能有问题[可能还不准确]
-    # wifi登录网关的账号后边还有@cau；lan连接的网关没用；
-    params1 = "/drcom/login?callback=dr1003&DDDDD="+logName + \
-        "%40cau&upass="+logPass+"&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip="
-    params2 = "/drcom/login?callback=dr1003&DDDDD="+logName + \
-        "&upass="+logPass+"&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip="
-    # print LoginURL[7:]
-    # return 0
-    url1 = urljoin(LoginURL, params1)
-    url2 = urljoin(LoginURL, params2)
-    res1 = requests.get(url1)
-    # 测试网络
-    netRes = testNet()
-    # 两个链接一个登录成功就行
-    if netRes['code'] == 302 and netRes['domain'] == LoginURL[7:]:
-        res2 = requests.get(url2)
-        netRes = testNet()
-    # 因为是用跳转链接测试，正常情况下，它永远返回302
-    if netRes['code'] == 302:
-        # 跳到微软，已经在登录状态
-        if netRes['domain'] == 'go.microsoft.com':
-            print "login success"
-        # 要跳到登录页
-        elif netRes['domain'] == LoginURL[7:]:
-            # 保存的信息不对。因为在netLogIn()运行之前，已经强制获取了ID和Pass信息
-            tmp = raw_input(
-                "ID or Password error or you don't have enough flow,do you want to correct your ID and Password?(y/n):")
-            if tmp.lower() == 'yes' or tmp.lower() == 'y':
-                data1 = checkPassFile(LoginURL, 0)
-                netLogIn(LoginURL, data1['ID'], data1['Pass'])
-        else:
-            print "sorry ,bye bye"
+            print("登录失败，请检查凭据")
+            return False
     else:
-        print "unkonwn error"
+        print("登录请求失败")
+        return False
 
+def check_login_status():
+    """验证登录状态"""
+    try:
+        response = requests.get("http://www.msftconnecttest.com/redirect", allow_redirects=False)
+        if response.status_code == 302:
+            location = response.headers.get("Location", "")
+            if "go.microsoft.com" in location:
+                return True  # 已经登录
+        return False
+    except requests.exceptions.RequestException:
+        return False
 
-def netLogOut(LoginURL):
-    params = "/drcom/logout?callback=dr1002"
-    url = urljoin(LoginURL, params)
-    res = requests.get(url)
-    print "Log Out"
-
+def network_logout(login_url):
+    """执行注销"""
+    # 解析登录URL的协议和主机部分
+    parsed_url = urllib.parse.urlparse(login_url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+    
+    # 构造注销URL
+    logout_url = f"{base_url}/drcom/logout"
+    logout_params = {
+        "callback": "dr1004",  # 注销回调函数
+        "v": "3690"           # 注销版本号
+    }
+    print(f"正在注销，使用注销地址: {logout_url}?{urllib.parse.urlencode(logout_params)}")
+    if secure_request(logout_url, logout_params):
+        print("注销成功")
+    else:
+        print("注销失败")
 
 if __name__ == '__main__':
-    displayAuthor()
-    loginurl = getLoginUrl()
-    data = checkPassFile(loginurl, 1)
-    if loginurl == 'logged':
-        # 检测到已经登陆，是否要退出？
-        tmp = raw_input(
-            "You have already logged in, do you want to log out? (y[es]/n[o], default no):")
-        if tmp.lower() == 'yes' or tmp.lower() == 'y':
-            try:
-                netLogOut(data['Url'])
-            except Exception, e:
-                print "Logout failed! You have to logout using your browser."
-                py_path = getPWD()
-                # 提取信息
-                f = open(py_path+'data.pkl', 'r')
-                data1 = pickle.load(f)
-                f.close()
-                # 重新写入信息
-                f = open(py_path+'data.pkl', 'wb')
-                data1['Url'] = "WrongURL"
-                pickle.dump(data1, f)
-                f.close()
-                fin()
-    elif data['ID'] != '' and data['Pass'] != '':
-        netLogIn(loginurl, data['ID'], data['Pass'])
-    else:
-        print "network error"
+    display_author()
+    
+    try:
+        # 检查是否已经登录
+        if check_login_status():
+            print("提示: 您已经登录了！")
+            while True:
+                choice = input("是否要注销？(Y/N, 默认 N): ").strip().lower()
+                if choice in ('y', 'yes'):  # 支持 y 和 yes
+                    # 获取上次使用的网关地址
+                    user_id = keyring.get_password(SERVICE_NAME, "last_user_id")
+                    if user_id:
+                        login_url = keyring.get_password(SERVICE_NAME, f"{user_id}_url")
+                        if login_url:
+                            network_logout(login_url)
+                        else:
+                            print("错误: 找不到存储的网关地址")
+                    else:
+                        print("错误: 找不到存储的用户ID")
+                    break
+                elif choice in ('n', 'no', ''):  # 支持 n、no 和直接回车
+                    print("未注销，退出程序。")
+                    break
+                else:
+                    print("无效输入，请输入 Y/N 或 Yes/No。")
+            sys.exit(0)
+        
+        # 获取认证网关地址
+        login_url = get_login_url()
+        if not login_url:
+            sys.exit(1)
+        
+        # 获取账号密码
+        user_id, password = get_credentials()
+        
+        # 尝试登录
+        if not network_login(login_url, user_id, password):
+            # 如果登录失败，清除存储的凭据并提示重新输入
+            print("登录失败，清除存储的凭据...")
+            keyring.delete_password(SERVICE_NAME, "last_user_id")
+            keyring.delete_password(SERVICE_NAME, user_id)
+            user_id, password = get_credentials()
+            network_login(login_url, user_id, password)
+        
+        # 存储网关URL
+        keyring.set_password(SERVICE_NAME, f"{user_id}_url", login_url)
+    except KeyboardInterrupt:
+        print("\n操作已终止")
+    finally:
+        # 内存安全清理
+        if 'password' in locals():
+            del password
+        sys.exit(0)
+        
